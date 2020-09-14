c79cdea659ca9ca3b6940d7760a5178c7bd72bd2
anderson-nanni <69260007+anderson-nanni@users.noreply.github.com>
2020-09-14 11:20:18 -0300

    js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 function gerar () {
-    
+    let num = document.getElementById('numero')
+    let tab = document.getElementById('tab')
+
+    if(num.value.length == 0) {
+        alert("Digite um número!")
+    } else {
+        let n = Number(num.value)
+        tab.innerText = ''
+
+        for (i = 1;i <= 10; i++) {
+            let item = document.createElement('option')
+            item.text += `${n} X ${i} = ${n*i}`
+            tab.appendChild(item)
+        }
+
+    }
 }
